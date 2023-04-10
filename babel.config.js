7ec393784211ceb4a06825e6eefd3270df1652a6
Konstantin Späth <duell10111@t-online.de>
2023-04-10 19:48:04 +0200

    chore: add metro and babel plugins

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    "@babel/plugin-transform-flow-strip-types",
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-proposal-export-namespace-from",
+    "@babel/plugin-syntax-import-assertions",
+    "@babel/plugin-proposal-private-methods",
+  ],
 };
