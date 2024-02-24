c3bef67d7e91d8701fc4547730c6e3e69e0a1680
Konstantin Späth <duell10111@t-online.de>
2024-02-24 01:15:47 +0100

    chore: first tests with native searchbar

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,9 +1,11 @@
+import {RnSearchBarView} from "@duell10111/rn-search-bar";
 import React from "react";
-import useSearchScreen from "../hooks/useSearchScreen";
-import SearchBar from "../components/search/SearchBar";
 import {View} from "react-native";
+
 import GridView from "../components/GridView";
+import SearchBar from "../components/search/SearchBar";
 import useGridColumnsPreferred from "../hooks/home/useGridColumnsPreferred";
+import useSearchScreen from "../hooks/useSearchScreen";
 
 export default function SearchScreen() {
   const {search, searchResult, fetchMore} = useSearchScreen();
@@ -15,8 +17,8 @@ export default function SearchScreen() {
   console.log(searchResult.length);
 
   return (
-    <>
-      <SearchBar onSubmit={text => search(text).catch(console.warn)} />
+    <View style={{flex: 1}}>
+      <RnSearchBarView color="red" style={{width: "100%", height: 400}} />
       <View style={{flex: 1}}>
         <GridView
           columns={columns}
@@ -24,6 +26,6 @@ export default function SearchScreen() {
           onEndReached={() => fetchMore().catch(console.warn)}
         />
       </View>
-    </>
+    </View>
   );
 }
