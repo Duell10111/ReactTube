44dfb0f029584c7efe34bf7ecd78a18ea4a76107
Konstantin Späth <duell10111@t-online.de>
2025-10-27 00:28:00 +0100

    chore: upgrade to expo sdk 54 new architecture

--- metro.config.js
+++ metro.config.js
@@ -1,30 +1,12 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const {getDefaultConfig} = require("expo/metro-config");
 
-const appJSONConfig = require("./app.json");
+// const appJSONConfig = require("./app.json");
 
 // eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname);
 
-// When enabled, the optional code below will allow Metro to resolve
-// and bundle source files with TV-specific extensions
-// (e.g., *.ios.tv.tsx, *.android.tv.tsx, *.tv.tsx)
-//
-// Metro will still resolve source files with standard extensions
-// as usual if TV-specific files are not found for a module.
-//
-/*
-if (process.env?.EXPO_TV === '1') {
-  const originalSourceExts = config.resolver.sourceExts;
-  const tvSourceExts = [
-    ...originalSourceExts.map((e) => `tv.${e}`),
-    ...originalSourceExts,
-  ];
-  config.resolver.sourceExts = tvSourceExts;
-}
- */
-
-if (appJSONConfig.expo.plugins[0][1].isTV) {
+if (process.env.EXPO_TV) {
   console.log("---- Adding TV file extensions ----");
   config.resolver.sourceExts.unshift(
     ...config.resolver.sourceExts.map(e => `tv.${e}`),
@@ -33,7 +15,5 @@ if (appJSONConfig.expo.plugins[0][1].isTV) {
 
 config.resolver.sourceExts.push("sql");
 config.resolver.unstable_enablePackageExports = true;
-
-// console.log(config.resolver.sourceExts);
 
 module.exports = config;
---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  project: {
+    android: {
+      unstable_reactLegacyComponentNames: ["Video"],
+    },
+    ios: {
+      unstable_reactLegacyComponentNames: ["Video"],
+    },
+  },
+};
